865cb39fe4f40817d77ae46b5257e1958e027979
PalindromeLeung <isprime67@gmail.com>
2022-12-12 12:28:03 -0600

    constructing PBTree

--- PathORAMcoq/PathORAM.v
+++ PathORAMcoq/PathORAM.v
@@ -2,60 +2,111 @@ Require Import List.
 Import ListNotations.
 From Coq Require Import Lia.
 Require Import Coq.Bool.Bool.
+Require Import Coq.Arith.EqNat.
+Require Import Coq.Init.Datatypes.
+Require Import Coq.Strings.String.
+Require Import Coq.NArith.BinNat. 
 
 Section Utils.
-  Fixpoint leb (n m : nat) : bool :=
-  match n with
-  | O => true 
-  | S n' => 
-      match m with
-      | O =>  false
-      | S m' => leb n' m'
-      end
-  end.
-  
+
 End Utils.
 
 Section Tree.
-
   
   Inductive PBTree (A: Type) : Type :=
-  | Leaf (idx: nat) (val: list A)
-  | Node (idx: nat) (val: list A) (l r: PBTree A).
+  | Leaf (idx: nat) (val: A)
+  | Node (idx: nat) (val: A) (l r: PBTree A).
 
-  Fixpoint getHeight (A: Type) (root: PBTree A) : nat :=
+  Arguments Leaf {_} _ _.
+  Arguments Node {_} _ _ _ _.
+
+  Fixpoint getHeight {A: Type} (root: PBTree A) : nat :=
     match root with
-    | Leaf _  _ _ => 1
-    | Node _ _ _ l r => if(leb (getHeight A l) (getHeight A r))then S(getHeight A r) else S(getHeight A l)
+    | Leaf _ _ => 0
+    | Node _ _ l r => S (max (getHeight l) (getHeight r))
     end.
 
-  (*TODO getPath & clearPath*)
-  
+  Fixpoint buildPBTlevelOrder {A} (def_a : A) (label : nat) (ht : nat) : PBTree A :=
+    match ht with
+    | 0 => Leaf label def_a
+    | S ht' => Node label def_a
+                      (buildPBTlevelOrder def_a (2 * label + 1) ht')
+                      (buildPBTlevelOrder def_a (2 * label + 2) ht')
+    end.
+
+  Definition PBTree1 {A} : PBTree (list A) := buildPBTlevelOrder [] 0 3.
+
+  Inductive Dir := L | R.
+
+  (* path needed to reach lfIdx in a BFS-numbered binary tree *)
+  Definition getPath (lfIdx : nat) : list Dir.
+  Admitted.
+
+  Fixpoint writeAtPath {A} (root : PBTree A) (path : list Dir) (data : A) : PBTree A :=
+    match root with
+    | Leaf idx val => match path with
+                     | [] => Leaf idx data
+                     | _ => Leaf idx val (* path longer than height of tree*)
+                     end
+    | Node idx val l r => match path with
+                         | [] => Node idx data l r
+                         | L :: path' => Node idx val (writeAtPath l path' data) r
+                         | R :: path' => Node idx val l (writeAtPath r path' data)
+                         end
+    end.
+
+  Definition writeToNode {A} (root : PBTree A) (lfIdx : nat) (tgtl : nat) (data : A) : PBTree A := writeAtPath root (firstn tgtl (getPath lfIdx)) data.
+
+  (*
+  Fixpoint writeToNode {A} (root : PBTree A) (lfIdx : nat) (tgtl : nat) (data : A) : PBTree A :=
+    match tgtl with
+    | 0 => match root with
+          | Leaf idx _ => Leaf idx data
+          | Node idx _ l r => Node idx data l r
+          end
+    | S tgtl => match root with
+               | Leaf _ _ => root
+               | Node idx val l r => if isLeft lfIdx tgtl 
+    *)  
 End Tree.
 
 
-Section Fold.
-  Fixpoint foldr {A : Type} {B : Type} (f : A -> B -> B) (v : B) (l : list A) := 
-  match l with
-  | nil => v
-  | x :: xs => f x (foldr f v xs)
-  end.
 
-  Fixpoint foldl {A : Type} {B : Type} (f : B -> A -> B) (v : B) (l : list A) :=
-  match l with
-  | nil => v
-  | x :: xs => foldl f (f v x) xs
-  end.
-
-End Fold.
 
 
 Section PathORAM.
   Definition nodesTotal := 28.
   Definition nodecap := 4.
 
-  Inductive Operation :=
+  Inductive Op :=
   | Rd
   | Wr.
+
   
- End PathORAM.
+End PathORAM.
+
+
+Section PathORAM.
+  Fixpoint ValEq (a : nat) (b : nat): Prop := eq_nat a b.
+  Definition dataNone := 45.
+
+  Fixpoint access (op : Op) (blockId : nat) (data: nat): nat . Admitted.
+
+  Theorem PathORAMValid: forall (data: nat)(blockId: nat),
+    AccessStream (access Wr blockId data) (access Rd blockId dataNone),
+    ValEq data (access Rd blockId dataNone).
+    
+  Admitted.
+
+  
+  
+  Theorem PathORAMSecure :
+    forall (i: nat) (oplista: list Operation)(oplistb: list Operation)
+      (datalista: list nat) (datalistb: list nat) 
+      (blocklista: list nat) (blocklistb list nat)
+      (a : list (access (fetchlist oplista i) (fetchlist blocklista i ) (fetchlist datalista i)))
+      (b : list (access (fetchlist oplistb i) (fetchlist blocklistb i ) (fetchlist datalistb i))), comp_indistinguish a b.
+
+    
+  
+End PathORAM.
