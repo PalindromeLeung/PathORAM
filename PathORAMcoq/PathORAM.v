9222386719b136951033169c8bb1c08c669bbf95
PalindromeLeung <isprime67@gmail.com>
2023-01-30 18:55:12 -0600

    initialization function

--- PathORAMcoq/PathORAM.v
+++ PathORAMcoq/PathORAM.v
@@ -11,6 +11,10 @@ Require Import Coq.Program.Wf.
 Require Import Streams.
 Require Import Coq.ZArith.Zdiv.
 
+Require Import  ExtLib.Data.Monads.StateMonad ExtLib.Structures.Monads.
+    
+
+
 Section Utils.
 
 End Utils.
@@ -30,6 +34,15 @@ Section Tree.
     | Node _ _ l r => S (max (getHeight l) (getHeight r))
     end.
 
+
+  Fixpoint isPBTree {A} (t : PBTree A) : Prop :=
+    match t with
+    | Leaf _ _ => True
+    | Node _ _ l r => getHeight l = getHeight r
+                     /\ (isPBTree l) /\( isPBTree r)
+    end.
+  
+                       
   Fixpoint buildPBTlevelOrder {A} (def_a : A) (label : nat) (ht : nat) : PBTree A :=
     match ht with
     | 0 => Leaf label def_a
@@ -38,23 +51,12 @@ Section Tree.
                       (buildPBTlevelOrder def_a (2 * label + 2) ht')
     end.
 
+  
   Definition PBTree1 {A} : PBTree (list A) := buildPBTlevelOrder [] 0 3.
 
   Compute PBTree1.
   
   Inductive Dir := L | R.
-
-  (* path needed to reach lfIdx in a BFS-numbered binary tree *)
-  (* Definition getPath (lfIdx : nat) : list Dir.  *)
-
-
-
-  (* 1 goal (ID 92) *)
-  
-  (* idx : nat *)
-
-  (* ============================ *)
-  (* Nat.pred (Nat.div idx 2) < S idx *)
 
   Lemma div2 : forall (n : nat), PeanoNat.Nat.div2 n  < S n.
   Proof.
@@ -82,8 +84,10 @@ Section Tree.
   apply div2.
   Defined.
 
+  Compute getPath 9.
   Definition p9 := getPath 9.
 
+  Compute getPath 12.
   Definition p12 := getPath 12.
 
   Fixpoint buildNodeLevelDict {A} (root: PBTree A) (currL : nat) : list (prod nat nat) :=
@@ -95,17 +99,20 @@ Section Tree.
 
   Compute buildNodeLevelDict PBTree1 0.
 
-  Fixpoint writeAtPath {A} (root : PBTree A) (path : list Dir) (data : A) : PBTree A :=
+  Fixpoint writeAtPath {A} (root : PBTree A)
+           (path : list Dir) (data : A) : PBTree A :=
     match root with
-    | Leaf idx val => match path with
-                     | [] => Leaf idx data
-                     | _ => Leaf idx val (* path longer than height of tree*)
-                     end
-    | Node idx val l r => match path with
-                         | [] => Node idx data l r
-                         | L :: path' => Node idx val (writeAtPath l path' data) r
-                         | R :: path' => Node idx val l (writeAtPath r path' data)
-                         end
+    | Leaf idx val =>
+        match path with
+        | [] => Leaf idx data
+        | _ => Leaf idx val (* path longer than height of tree*)
+        end
+    | Node idx val l r =>
+        match path with
+        | [] => Node idx data l r
+        | L :: path' => Node idx val (writeAtPath l path' data) r
+        | R :: path' => Node idx val l (writeAtPath r path' data)
+        end
     end.
 
   Compute writeAtPath PBTree1 p12 [1;2;3].
@@ -122,15 +129,15 @@ Section Tree.
   CoFixpoint rand (seed n1 n2 : Z) : Stream Z :=
     let seed' := Zmod seed n2 in Cons seed' (rand (seed' * n1) n1 n2).           
 
-  Fixpoint take {X} n (str : Stream X) : list X :=
+  Fixpoint takeS {X} n (str : Stream X) : list X :=
     match n with
     | 0 => []
     | S m => match str with
-            | Cons x str' => x :: take m str'
+            | Cons x str' => x :: takeS m str'
             end
     end.
 
-  Definition first60 := take 60(rand 475 919 953).
+  Definition first60 := takeS 60(rand 475 919 953).
 
   Definition md_tot := fun x => Zmod x 15.
 
@@ -155,13 +162,6 @@ Section Tree.
                   then (key, val :: al) :: t 
                   else (n, al) :: aggregation_helper key val t
     end.
-
-(*   defaultdict(<class 'int'>, 
-{1: 7, 2: 13, 3: 8, 4: 13, 5: 14, 6: 12, 7: 7, 8: 7, 9: 8, 10: 13, 11: 9, 12: 3, 13: 2, 14: 12, 15: 8, 16: 7, 17: 10, 18: 9, 19: 12, 20: 2, 21: 1, 22: 7, 23: 4, 24: 2, 25: 1, 26: 8, 27: 12, 28: 14}) *)
-
-
-(* defaultdict(<class 'list'>, 
-{7: [1, 7, 8, 16, 22], 13: [2, 4, 10], 8: [3, 9, 15, 26], 14: [5, 28], 12: [6, 14, 19, 27], 9: [11, 18], 3: [12], 2: [13, 20, 24], 10: [17], 1: [21, 25], 4: [23]}) *)
            
   Fixpoint aggregation (l : list (nat * nat)): list (nat * (list nat)):=
     match l with
@@ -170,26 +170,85 @@ Section Tree.
     end.
 
   Compute aggregation [(1, 3); (2,3); (8, 3); (4, 4); (5,4)].
+  Definition n_bl_pair := aggregation [(1, 3); (2,3); (8, 3); (4, 4); (5,4)].
 
   Fixpoint makeNZeros (n : nat) : list nat :=
     match n with
     | O => []
     | S n' => O :: makeNZeros n'
     end.
-  
-  Check prod.
-  Fixpoint pairGen (l : list(nat * list  nat)) : list (nat * nat) :=
-    match l with 
-    | nil => []
-    | (n, al) :: t => List.combine al (makeNZeros (List.length al))
+
+  Compute makeNZeros 10.
+
+
+  Fixpoint takeL {A} n (l : list A) : list A :=
+    match n with
+    | O => []
+    | S m => match l with
+            | [] => []
+            | h :: t => h :: takeL m t 
+            end
     end.
 
-  (* Fixpoint initialize_tree {A} {B} {C}(root : PBTree A) (stsh : list (prod B C)) : PBTree A := *)
-  (*   match root with *)
-  (*   | Leaf idx val =>  *)
-  (*   | Node idx val l r =>  *)
+  Compute takeL 4 [1;2;3].
+  
+  Fixpoint takeFromIdx {A} (n : nat) (l : list A)  : list A :=
+    match l with
+    | [] => []
+    | h :: t => match n with
+              | O => h :: t
+              | S m => takeFromIdx m t
+              end
+    end.
+
+  Compute takeFromIdx 2 [1; 2; 3;4].
+  
+  
+  Fixpoint pairGen {A} (l : list(nat * list nat)) (rt : PBTree A ): list (nat * nat) :=
+    match l with
+    | [] => []
+    | (n, bs) :: t => match rt with
+                    | Leaf idx _ => if Nat.eqb n idx
+                                   then List.combine bs (makeNZeros (List.length bs))
+                                                     (* expand this n's bs *)
+                                   else pairGen t rt (* recurse  *)
+                    | Node idx _ l r => if Nat.eqb n idx
+                                       then List.combine bs (makeNZeros (List.length bs))
+                                                         (* expand this n's bs *)
+                                       else pairGen t rt  (* recurse  *)
+                    end
+    end.
+
+  Compute pairGen n_bl_pair (Leaf 3 3).
+  Compute takeL 2 (pairGen n_bl_pair (Leaf 3 3)). 
+
+  Check writeAtPath.
+  
+  Fixpoint initializeTree {A} (rt : PBTree A) (stsh : list (nat * nat))
+           (l : list(nat * list nat)): (PBTree A, list(nat * nat)) :=
+    
+    match rt with
+    | Leaf idx val => let data := pairGen l rt in
+                     let dataH := takeL data 4 in
+                     let dataT := takeFromIdx data 4 in
+                     let newStsh := stsh ++ dataT in
+                     (writeAtPath rt (getPath rt) dataH, newStsh)
+                       
+    | Node idx val lc rc => let data := pairGen l rt in
+                           match data with
+                           | [] => let dataH := takeL data 4 in
+                                  let dataT := takeFromIdx data 4 in
+                                  let newStsh := stsh ++ dataT in
+                                  (writeAtPath rt (getPath rt) dataH, newStsh)
+                           | _ => initializeTree lc stsh l \/ initializeTree rc stsh l
+                           end
+    end.
   
 End Tree.
+
+
+
+
 
 
 
@@ -219,15 +278,23 @@ Section PathORAM.
 
   Fixpoint access (op : Op) (blockId : nat) (data: nat): nat . Admitted.
 
-  Theorem PathORAMValid: forall (data: nat)(blockId: nat),
-    AccessStream (access Wr blockId data) (access Rd blockId dataNone),
-    ValEq data (access Rd blockId dataNone).
+  (* S -> S * X *)
+  (*           proj_2 () *)
+            
+  Theorem PathORAM_simulates_RAM: forall (data: nat)(blockId: nat),
+    bind (access Wr blockId data) (access Rd blockId dataNone) ->
+    ValEq data getData(access Rd blockId dataNone).
     
   Admitted.
 
   
   
-  Theorem PathORAMSecure :
+  Theorem PathORAMIsSecure :
+    forall (y : list Access) (z : list Access), 
+     comp_indistinguish (getPos(fold_LM y)) (getPos(fold_LM z)). 
+
+
+    
     forall (i: nat) (oplista: list Operation)(oplistb: list Operation)
       (datalista: list nat) (datalistb: list nat) 
       (blocklista: list nat) (blocklistb list nat)
