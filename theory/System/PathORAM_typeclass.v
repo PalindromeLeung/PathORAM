dd9010207ddb5fa9fa278cf0502ec6d7de5158a3
Sankar Gopalkrishna <sankarg2@illinois.edu>
2024-02-08 18:01:32 -0600

    fixed lookup_ret_val for return type as nat not list nat

--- theory/System/PathORAM_typeclass.v
+++ theory/System/PathORAM_typeclass.v
@@ -525,7 +525,13 @@ Definition dummy_block : block := Block O O.
 Definition dummy_path {l : nat} : path l := const_vec false l.
 
 
-Definition get_cand_bs {n l : nat} (o : oram n l) : list block := []. (* to be implemented *)
+Definition get_cand_bs {n l : nat} (o : oram n l) : list block := [] (* todo gi*)
+(*
+  match o with
+  | Leaf_ORAM => []
+  | Node_ORAM bkt my_n my_l =>
+  end.
+*)
 
 (* cap is the capability of each node, the current magic number is 4 based on the original paper *)
 Definition get_write_back_blocks {n l : nat} (o : oram n l) (cap : nat)  (h : stash n) (id : nat) : list block :=
@@ -541,11 +547,11 @@ Definition get_write_back_blocks {n l : nat} (o : oram n l) (cap : nat)  (h : st
 
 Definition remove_list_sub {A} (smL : list A) (p : A -> bool) (toL : list A) : list A := []. (* to be implemented *)
 
-Fixpoint lookup_ret_data (id : block_id) (lb : list block): list nat :=
+Fixpoint lookup_ret_data (id : block_id) (lb : list block): nat :=
   match lb with
-  | [] => []
+  | [] => 0
   | h :: t =>
-      if Nat.eqb (block_blockid h) id then block_payload h :: lookup_ret_data id t
+      if Nat.eqb (block_blockid h) id then block_payload (h%nat)
       else lookup_ret_data id t
   end.
 Definition example_block_list : list block := [
