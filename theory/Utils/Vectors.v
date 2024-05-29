921c99987a1e49d805d9d50d8e2e922d497a0cdb
Hannah Leung <isprime67@gmail.com>
2024-05-29 16:00:05 -0500

    move Vectors.v into theory/Utils folder

---
+++ theory/Utils/Vectors.v
@@ -0,0 +1,46 @@
+Require Coq.Vectors.Vector.
+Require Import POram.Utils.Classes.
+Import MonadNotation.
+
+(*** VECTORS ***)
+
+Definition head_vec {A : Type} {n : nat} (xs : Vector.t A (S n)) : A :=
+  match xs with
+  | Vector.cons _ x _ _ => x
+  end.
+
+Definition tail_vec {A : Type} {n : nat} (xs : Vector.t A (S n)) : Vector.t A n :=
+  match xs with
+  | Vector.cons _ _ _ xs => xs
+  end.
+
+Fixpoint zip_vec {A B : Type} {n : nat} : forall (xs : Vector.t A n) (ys : Vector.t B n), Vector.t (A * B) n :=
+  match n with
+  | O => fun _ _ => Vector.nil (A * B)
+  | S n' => fun xs ys =>
+      let x := head_vec xs in
+      let xs' := tail_vec xs in
+      let y := head_vec ys in
+      let ys' := tail_vec ys in
+      Vector.cons _ (x , y) _ (zip_vec xs' ys')
+  end.
+
+Fixpoint const_vec {A : Type} (x : A) (n : nat) : Vector.t A n :=
+  match n with
+  | O => Vector.nil A
+  | S n' => Vector.cons _ x _ (const_vec x n')
+  end.
+
+Fixpoint constm_vec {A : Type} {M : Type -> Type} `{Monad M} (xM : M A) (n : nat) : M (list A) :=
+  match n with
+  | O => mreturn (@nil A)
+  | S n' =>
+      x <- xM ;;
+      xs <- constm_vec xM n' ;;
+      mreturn (cons x xs)
+  end.
+
+Definition to_list_vec {A : Type} {n : nat} := @Vector.to_list A n.
+Definition map_vec {A B : Type} {n : nat} f := @Vector.map A B f n.
+
+#[export] Instance Functor_vec {n : nat} : Functor (fun A => Vector.t A n) := { map {_ _} f xs := map_vec f xs }.
