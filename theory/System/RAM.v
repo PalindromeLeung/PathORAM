f83fce8da273bfad19223d7d128808031c9342ef
Talia Ringer <talia@dependenttyp.es>
2024-05-31 12:03:29 -0500

    read-read law, first attempt (breaks implementations for now)

--- theory/System/RAM.v
+++ theory/System/RAM.v
@@ -24,12 +24,21 @@ Module Type RAM (M : StateMonad).
   (* Wrapped value type, specific to implementation *)
   Parameter Vw : Type -> Type.
 
+  (* Read and write operations *)
   Parameter read : K -> M.State St (Vw V).
   Parameter write : K -> V -> M.State St (Vw V).
 
-  (* TODO laws
-  Axiom read_read : ...
-  Axiom read_write : ... 
+  (* RAM laws (TODO maybe add uniform syntax here) *)
+  Axiom read_read :
+    forall (k : K), 
+      M.bind (read k) (fun _ => read k) =
+      M.bind (read k) (fun v => M.ret v). 
+
+  (* TODO remaining laws
+write(key,value) ; read(key)  == write(key,value) ; return(value) -- read-write law
+v1 <- read(key1) ; v2 <- read(key2) ; f(v1,v2) == v2 <- read(key2) ; v1 <- read(key1) ; f(v1,v2) -- read-commute law (doesn't require key1 =/= key2)
+v <- read(key1); write(key2,value); f(v) == write(key2,value) ; v <- read(key1) ; f(v) -- read-write-commute law (requires key1 =/= key2)
+write(key1,value1) ; write(key2,value2) == write(key2,value2) ; write(key1,value1) -- write-commute law (requires key1 =/= key2)
   *)
 
   (* TODO how does it relate to lens laws? *)
